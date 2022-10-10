0dd2a052f5907c0d222a1b1c46238b311b43fa8e
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2022-10-10 15:46:00 +0530

    done with use state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import "./App.css";
 import Header from "./components/Header";
-import Movie from "./components/Movie";
-import movies from "./data.json";
-
 // Cannot Change name and ordering
 // if we use small letter then we the compiler take it as a jsx element not as a component
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(1);
+  function inc() {
+    setNum(num + 1);
+  }
+  function dec() {
+    setNum(num - 1);
+  }
   return (
     <div className="App">
       <Header />
       <div className="main">
-        {movies.map((element, index) => {
-          return (
-            <Movie
-              key={index}
-              title={element.Title}
-              year={element.Year}
-              img={element.Poster}
-            />
-          );
-        })}
+        <h1 className="heading">{num}</h1>
+        <div className="buttons">
+          <button className="btn" onClick={inc}>
+            increment
+          </button>
+          <button className="btn" onClick={dec}>
+            decrement
+          </button>
+        </div>
       </div>
     </div>
   );
