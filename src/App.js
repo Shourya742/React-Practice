f6de0aa403aa37019d1e599f17a4fe55b48b2303
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2022-10-15 16:33:31 +0530

    useMemo

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import "./App.css";
 import Header from "./components/Header";
 // Cannot Change name and ordering
 // if we use small letter then we the compiler take it as a jsx element not as a component
-import { useState, useEffect } from "react";
-import useFetch from "./components/useFetch";
+import { useState, useEffect, useMemo } from "react";
+
 function App() {
-  const [data] = useFetch(
-    "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001"
-  );
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const expensiveCalculation = (num) => {
+    for (let i = 0; i < 10000000; i++) {}
+    return num;
+  };
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
   return (
     <>
       <div className="App">
         <Header />
-        {data.map((e, i) => {
-          return (
-            <div key={i}>
-              <h1>e.firstName</h1>
-              <h1>e.email</h1>
-            </div>
-          );
-        })}
+        <button onClick={() => setCount(count + 1)}>Incrment</button>
+        <h1>Count: {count}</h1>
+        <input onChange={(e) => setName(e.target.value)} />
+        <h1>Name: {name}</h1>
       </div>
     </>
   );
