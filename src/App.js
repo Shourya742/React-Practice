8990ab3b9ac6848fe0b7e995d025a8b99e031c81
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2022-10-15 15:30:22 +0530

    UseContext API

--- src/App.js
+++ src/App.js
@@ -2,40 +2,25 @@ import "./App.css";
 import Header from "./components/Header";
 // Cannot Change name and ordering
 // if we use small letter then we the compiler take it as a jsx element not as a component
-import { useState, useEffect } from "react";
-import Home from "../src/components/Home.js";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import About from "../src/components/About";
-import Contact from "../src/components/Contact";
-import Error from "./components/Error";
-import Insta from "./components/Insta";
-import Mail from "./components/Mail";
-import Userdetails from "./components/Userdetails";
+import ComA from "./components/ComA";
+import { useState, createContext } from "react";
+
+const AppState = createContext();
+
 function App() {
+  const [data, setData] = useState("Huhahah");
+  const [name, setName] = useState({ name: "Shourya", age: "infinity" });
   return (
-    <Router>
-      <div className="App">
-        <Header />
-
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="/app" element={<Home />} />
-            <Route path="/app/:userId" element={<Userdetails />} />
-          </Route>
-
-          <Route path="/about" element={<About />} />
-
-          <Route path="/contact" element={<Contact />}>
-            <Route index element={<Insta />} />
-            <Route path="insta" element={<Insta />} />
-            <Route path="mail" element={<Mail />} />
-          </Route>
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <AppState.Provider value={{ data, name }}>
+        <div className="App">
+          <Header />
+          <ComA />
+        </div>
+      </AppState.Provider>
+    </>
   );
 }
 
 export default App;
+export { AppState };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,7 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  NavLink,
-} from "react-router-dom";
 const Header = () => {
-  const navLinkStyles = ({ isActive }) => {
-    return {
-      textDecoration: isActive ? "none" : "underline",
-      color: isActive ? "red" : "white",
-      fontSize: "25px",
-    };
-  };
   return (
-    <div className="header">
-      <NavLink to={"/app"} style={navLinkStyles}>
-        Home
-      </NavLink>
-      <NavLink to={"/about"} style={navLinkStyles}>
-        About
-      </NavLink>
-      <NavLink to={"/contact"} style={navLinkStyles}>
-        Contact
-      </NavLink>
+    <div className="header" style={{ color: "white" }}>
+      <h1>React Context Api</h1>
     </div>
   );
 };
